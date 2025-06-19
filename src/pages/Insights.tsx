b8d011c8501b5beb74f5d85211d06b98c78e5e95
Dickson Daniel Peprah <techpepson@gmail.com>
2025-06-19 16:16:28 +0000

    feat(update import): import the import in the inside file to correct the import error.

--- src/pages/Insights.tsx
+++ src/pages/Insights.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import Footer from "@/components/Footer";
 import {
   Card,
